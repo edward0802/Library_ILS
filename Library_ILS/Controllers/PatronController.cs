c4979ae913a151e75a94568e58e5fd725b643e6d
edward0802 <edik.kucherniuk@gmail.com>
2018-09-10 15:49:21 +0300

    Added Views/Patron/Detail and Index Views and appropriate methods in PatronController

--- Library_ILS/Controllers/PatronController.cs
+++ Library_ILS/Controllers/PatronController.cs
@@ -40,5 +40,30 @@ namespace Library_ILS.Controllers
             return View(model);
         }
 
+
+        public IActionResult Detail(int id)
+        {
+            var patron = _patron.Get(id);
+
+            var model = new PatronDetailModel
+            {
+                Id = patron.Id,
+                LastName = patron.LastName,
+                FirstName = patron.FirstName,
+                Address = patron.Address,
+                HomeLibraryBranch = patron.HomeLibraryBranch?.Name, // we can create checking:  patron.HomeLibraryBranch?.Name ?? "No Home Library",
+                MemberSince = patron.LibraryCard.Created,
+                OverdueFees = patron.LibraryCard.Fees,
+                LibraryCardId = patron.LibraryCard.Id,
+                Telephone = patron.TelephoneNumber,
+                AssetsCheckedOut = _patron.GetCheckouts(id).ToList(),
+                CheckoutHistory = _patron.GetCheckoutHistory(id),
+                Holds = _patron.GetHolds(id)
+            };
+
+            return View(model);
+        }
+
+
     }
 }
